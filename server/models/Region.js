e19c19d4be7dee5fbf1e080338c7139fb7b7d7f9
Besufekad-HAZ <besurye@gmail.com>
2025-07-30 13:40:04 +0300

    Create Region model

---
+++ server/models/Region.js
@@ -0,0 +1,16 @@
+const RegionSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  manager: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  workHourRules: {
+    startTime: String,
+    endTime: String,
+    maxHoursPerDay: Number
+  },
+  payRates: {
+    standard: Number,
+    overtime: Number
+  },
+  language: { type: String, default: 'en' }
+});
+
+module.exports = mongoose.model('Region', RegionSchema);
