9834a0be8af1652fac6e98019b4996ac5f6d878b
Besufekad-HAZ <besurye@gmail.com>
2025-12-19 16:26:00 +0300

    Fix bot avatar issue and width automation

--- client/components/Chatbot/BotAvatar.tsx
+++ client/components/Chatbot/BotAvatar.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Image from "next/image";
-import { AvatarProps } from "@/chatbot/config";
 import { resolveAssetUrl } from "@/lib/assets";
 
 const BOT_AVATAR_SRC =
   resolveAssetUrl("/belimuno-logo.png") ?? "/belimuno-logo.png";
 
-const BotAvatar: React.FC<AvatarProps> = () => {
+const BotAvatar: React.FC = () => {
   return (
-    <div className="bg-white w-[40px] rounded-full flex items-center justify-center">
+    <div className="bg-white w-10 rounded-full flex items-center justify-center">
       <Image
         src={BOT_AVATAR_SRC}
         alt="chatbot"
